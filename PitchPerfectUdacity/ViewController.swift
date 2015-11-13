4d98d4f751aea86810c947c6a80ff3d4836cf27e
Yeh-chi Lai <yehchilai@csupomona.edu>
2015-11-13 11:12:08 +0800

    Update image button action

--- PitchPerfectUdacity/ViewController.swift
+++ PitchPerfectUdacity/ViewController.swift
@@ -20,6 +20,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(sender: UIButton) {
+        //TODO: show text "recording in progress"
+        //TODO: record the user's voice
+        print("in recordAudio...");
+        
+    }
 
 }
 
